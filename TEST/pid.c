83070495596d08a899d1b7bd23d676c06e5f3a27
mkryan00 <13073@holbertonstudents.com>
2026-04-13 01:43:53 +0000

    Added PPID to pid.c

--- TEST/pid.c
+++ TEST/pid.c
@@ -9,8 +9,13 @@
 int main(void)
 {
     pid_t my_pid;
-
+    pid_t parent_pid;
+   
     my_pid = getpid();
-    printf("%u\n", my_pid);
+    parent_pid = getppid();
+    
+    printf("PID: %u\n", my_pid);
+    printf("PPID: %u\n", parent_pid);
+    
     return (0);
 }
