2db7f5fc4ccc2aeb2b6d023f7841ff46aa7d98ce
LachyBM <13072@holbertonstudents.com>
2026-04-15 05:39:59 +0000

    path??

--- shell.c
+++ shell.c
@@ -20,24 +20,79 @@ char *split(char *line, char **argv)
 	return (str);
 }
 
+char *the_path(char *path)
+{
+	char *path_env, *path_copy, *piece;
+	int j = 0;
+	char len[1024];
+
+	if (!path)
+		return (NULL);
+
+	if (access(path,X_OK) == 0)
+		return strdup(path);
+	/**
+	 * walks through environ for PATH=
+	 * if not exit
+	 */ 
+	while (environ[j])
+	{
+		if (strncmp(environ[j], "PATH=", 5) == 0)
+		{
+			path_env = environ[j] + 5;
+			break;
+		}
+		j++;
+	}
+	
+	if (!path_env)
+	{
+		printf("PATH NOT SET\n");
+		return (NULL);
+	}
+	path_copy = strdup(path_env);
+	piece = strtok(path_copy, ":");
+	while (piece)
+	{
+			sprintf(len,"%s/%s", piece, path);
+
+			if(access(len,X_OK) == 0)
+			{
+				free(path_copy);
+				return strdup(len);
+			}
+			piece = strtok(NULL, ":");
+	}
+		
+ 	free(path_copy);
+	return (NULL);
+}
+
 int cmd(char **argv)
 {
 	pid_t child_pid;
 	int status;
+	char *path;
 
+	path = the_path(argv[0]);
+	if (!path)
+		return (1);
 	child_pid = fork();
 	if (child_pid == -1)
 	{
 		perror("Error:");
+		free(path);
 		return (1);
 	}
 	if (child_pid == 0)
 	{
-		execve(argv[0], argv, environ);
+		execve(path, argv, environ);
 		perror("Error:");
+		free(path);
 		exit(1);
 	}
 	wait(&status);
+	free(path);
 	return (0);
 }
 
